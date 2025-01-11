3e74907067d2f6f8a755e0212a4e2e1cd2dd94a6
vikram kumar <vikramkumar8t8@gmail.com>
2025-01-11 17:41:13 +0500

    folder

---
+++ pages/_app.tsx
@@ -0,0 +1,29 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import './globals.css'
+import { LanguageProvider } from '@/contexts/LanguageContext'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+export const getServerSideProps = () => {
+  return {
+    props: {}
+  }
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Zero GPT</title>
+      </Head>
+      <LanguageProvider>
+        <Navbar />
+        <div key={Math.random()}>
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </LanguageProvider>
+    </>
+  )
+}
