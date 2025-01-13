3880658f22e9afcf31b866a5eed9a1f7ba99b7f9
vikram kumar <vikramkumar8t8@gmail.com>
2025-01-13 18:02:45 +0500

    about-us

--- components/AboutUs/EmbraceAIDetection.tsx
+++ components/AboutUs/EmbraceAIDetection.tsx
@@ -5,7 +5,7 @@ const EmbraceAIDetection = () => {
         <div className='py-10 sm:py-16 md:py-[90px] px-3.5 sm:px-5'>
             <div className='py-12 sm:pt-[68px] px-4 sm:pb-[78px] text-white text-center max-w-[1280px] mx-auto rounded-[20px] sm:rounded-[30px]' style={{background: "linear-gradient(180.17deg, #0E0F11 15.64%, #192C5C 99.86%)"}}>
                 <h1 className='text-[24px] sm:text-[32px] md:text-[40px] uppercase font-bold'>Embrace AI Detection</h1>
-                <p className='mt-3 max-w-[1050px] mx-auto text-sm sm:text-lg font-normal sm:leading-[26px]'>Join us at ZeroGPT.net and discover the power of our AI content detection tool. Stay one step ahead in the ever-evolving landscape of AI-generated content and make informed decisions based on accurate and reliable information. Together, let's navigate the AI revolution and embrace the future of content detection.</p>
+                <p className='mt-3 max-w-[1050px] mx-auto text-sm sm:text-lg font-normal sm:leading-[26px]'>Join us at ZeroGPT.net and discover the power of our AI content detection tool. Stay one step ahead in the ever-evolving landscape of AI-generated content and make informed decisions based on accurate and reliable information. Together, let&apos;s navigate the AI revolution and embrace the future of content detection.</p>
                 <button className='mt-5 sm:mt-7 bg-bright-cyan transition-all duration-300 ease-in-out hover:bg-soft-cyan hover:shadow-lg hover:text-white rounded-[30px] h-[40px] sm:h-[52px] px-7 text-dark-blue font-semibold text-sm sm:text-base uppercase'>Try It Now</button>
             </div>  
         </div>
